4ba3492d42e981ba527cef4c5b1a44477ce8d061
ParthNaykude <164043141+ParthNaykude@users.noreply.github.com>
2024-04-01 22:04:48 +0530

    Add files via upload
    c++-data-types-code

---
+++ c++-data-types-practice..cpp
@@ -0,0 +1,26 @@
+//c++-data-types-practice
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+
+    int integerNumber = 10;
+    char character = 'A';
+    long longNumber = 1234567890;
+    float floatNumber = 3.14;
+    double doubleNumber = 3.14159;
+    bool booleanValue = true;
+    string stringValue = "Hello World";
+    
+    cout << "Integer:" << integerNumber << endl;
+    cout << "Character: " << character << endl;
+    cout << "Long: " << longNumber << endl;
+    cout << "Float: " << floatNumber << endl;
+    cout << "Double: " << doubleNumber << endl;
+    cout << "Boolean: " << booleanValue << endl;
+    cout << "String: " << stringValue << endl;
+    
+    return 0;
+}
